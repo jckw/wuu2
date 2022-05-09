1f0791f00e4973daaad4ed26237a83a984e6aa69
Jack W <jackweatherilt@outlook.com>
2022-05-09 10:17:51 +0100

    Remove api.

--- api/index.js
+++
@@ -1,6 +0,0 @@
-const { createRequestHandler } = require('@remix-run/vercel')
-
-module.exports = createRequestHandler({
-  // eslint-disable-next-line global-require
-  build: require('./_build'),
-})
